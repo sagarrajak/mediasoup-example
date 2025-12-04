b2c66029897cdef71c2b4d63ac0ecac4a27bb51e
donkey king senior <sagarrajak858@gmail.com>
2025-12-04 19:15:49 +0530

    current work in progress

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,37 @@
-const Config = {
+import { WorkerLogLevel, WorkerLogTag } from "mediasoup/types";
+
+type WorkerSettings = {
+    logLevel: WorkerLogLevel,
+    logTags: WorkerLogTag[],
+    rtcMinPort: number,
+    rtcMaxPort: number
+}
+
+const Config: {
+    port: number,
+    workerSettings: WorkerSettings
+} = {
   port: 3030,
   workerSettings: {
-    logLevel: "error",
+    logLevel: "warn",
+    rtcMinPort: 40000,
+    rtcMaxPort: 41000,
+    logTags: [
+      "info",
+      "ice",
+      "dtls",
+      "rtp",
+      "srtp",
+      "rtcp",
+      "rtx",
+      "bwe",
+      "score",
+      "simulcast",
+      "svc",
+      "sctp",
+    ],
   },
 };
 
 export default Config;
+
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
+import express from "express";
 import fs from "fs";
 import https from "https";
-import express from "express";
+import { AppData, Router, Worker } from "mediasoup/types";
 import { Server } from "socket.io";
-import mediasoup from "mediasoup";
-import createWorkerHelper from "./createWorkerHelper";
+import Config from "./config.js";
+import createWorkerHelper from "./createWorkerHelper.js";
 
 const app = express();
 app.use(express.static("public"));
@@ -17,18 +18,40 @@ const httpsServer = https.createServer(options, app);
 
 const io = new Server(httpsServer, {
   cors: {
-    origin: "https://localhost:3030",
+    origin: `https://localhost:${Config.port}`,
     credentials: true,
   },
 });
 
-let workers = null;
+let workers: Worker<AppData>[] | null = null;
+let router: Router<AppData> | undefined =  undefined;
+
+
+const mediaCodecs = [
+  {
+    kind: "audio",
+    mimeType: "audio/opus",
+    clockRate: 48000,
+    channels: 2,
+  },
+  {
+    kind: "video",
+    mimeType: "video/H264",
+    clockRate: 90000,
+    parameters: {
+      "packetization-mode": 1,
+      "profile-level-id": "42e01f",
+      "level-asymmetry-allowed": 1,
+    },
+  },
+];
 
 // Make mediusoup configuration ready
 const initMediaSoup = async () => {
   workers = await createWorkerHelper();
+  router = await workers[0]?.createRouter({ mediaCodecs: mediaCodecs as any });
 };
 
 initMediaSoup();
 
-httpsServer.listen(3030);
+httpsServer.listen(Config.port);
